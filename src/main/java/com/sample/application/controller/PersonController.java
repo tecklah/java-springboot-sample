cb3af07037334e9120b8d4d10eeec2c8b87720a9
Cheng Soon Teck <SoonTeck_Cheng@Singaporeair.com.sg>
2019-08-05 17:32:02 +0800

    Update.

--- src/main/java/com/sample/application/controller/PersonController.java
+++ src/main/java/com/sample/application/controller/PersonController.java
@@ -1,6 +1,12 @@
 package com.sample.application.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +17,12 @@ public class PersonController {
 	      return "Hello World";
 	}
 
-	@RequestMapping(value = "/object")
-	public Object object(@RequestBody Object object) {
-	      return object;
+	@RequestMapping(value = "/object", method = RequestMethod.GET)
+	public Object object(@RequestHeader Map<String, String> headers, HttpServletRequest request) {
+			System.out.println("Addr:" + request.getRemoteAddr());
+			System.out.println("Host:" + request.getRemoteHost());
+			System.out.println("Port:" + request.getRemotePort());
+	      return headers;
 	}
 
 }
